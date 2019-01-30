a830920b7fed7c69b164983248ce13a305fda552
SS-Cat <ivensjorisnbacon@gmail.com>
2019-01-30 12:07:20 -0300

    adicionado validação dos campos com express-validator

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,11 +1,26 @@
 module.exports = function name(app) {
 	app.get(`/formulario_inclusao_noticia`, function (req, res) {
-		res.render(`admin/form_add_noticia`);
+		res.render(`admin/form_add_noticia`, {validation: ``, noticia: ``});
 	});
 
 	app.post(`/noticias/save`, function (req, res) {
 		const news = req.body;
 		// res.send(news);
+		// console.log(news);
+
+		req.assert(`titulo`, `Título é Obrigatório`).notEmpty();
+		req.assert(`resumo`, `Resumo é Obrigatório`).notEmpty();
+		req.assert(`resumo`, `Resumo deve conter entre 10 e 100 caracteres`).len(10, 100);
+		req.assert(`autor`, `Autor é Obrigatório`).notEmpty();
+		req.assert(`data_noticia`, `Data é Obrigatória`).notEmpty()/*.isDate({format: `YYYY-MM-DD`})*/;
+		req.assert(`noticia`, `Noticia é Obrigatório`).notEmpty();
+
+		const error = req.validationErrors();
+		if(error) {
+			res.render(`admin/form_add_noticia`, {validation: error, noticia: news});
+			return;
+		}
+
 		const connection = app.config.dbConnection();
 		const noticiasModel = new app.app.models.NoticiasDAO(connection);
 		
--- config/server.js
+++ config/server.js
@@ -1,11 +1,13 @@
-let express = require(`express`)();
-let consign = require(`consign`);
-let bodyParser = require(`body-parser`);
+const express = require(`express`)();
+const consign = require(`consign`);
+const bodyParser = require(`body-parser`);
+const expressValidator = require(`express-validator`);
 
 express.set(`view engine`, `ejs`);
 express.set(`views`, `./app/views`);
 
 express.use(bodyParser.urlencoded({extended: true}));
+express.use(expressValidator());
 
 consign()
 	.include(`./app/routes`)
